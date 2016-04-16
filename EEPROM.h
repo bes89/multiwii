86a07d605b7e8bba4bf3797ca13ed56aef4c2ddb
Feram <feram@adriansieber.com>
2016-04-16 18:41:03 +0000

    Fix typos in EEPROM.h

--- EEPROM.h
+++ EEPROM.h
@@ -15,7 +15,7 @@ void writePLog(void);
 
 void storeWP(void);     // Stores the WP data in the wp struct in the EEPROM
 bool recallWP(uint8_t); // Read the given number of WP from the eeprom, supposedly we can use this during flight.
-                        // Returns true when reading is successfull and returns false if there were some error (for example checksum)
+                        // Returns true when reading is successful and returns false if there were some error (for example checksum)
 uint8_t getMaxWPNumber(void);  // Returns the maximum WP number that can be stored in the EEPROM, calculated from conf and plog sizes, and the eeprom size
 
 void loadGPSdefaults(void);
